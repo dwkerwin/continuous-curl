d5f3163064cfc6f2c9e652c80182fcb3df83662f
Doug Kerwin <dwkerwin@gmail.com>
2018-09-11 14:53:21 -0400

    added color coding on 4xx, 5xx

--- ccurl.py
+++ ccurl.py
@@ -4,6 +4,16 @@ import time
 import subprocess
 
 
+class colors:
+    BLUE = '\033[94m'
+    CYAN   = '\033[36m'
+    YELLOW = '\033[93m'
+    RED = '\033[31m'
+    ENDCOLOR = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
 print("Ccurl (Continuous Curl)")
 if len(sys.argv) <= 1:
     print("USAGE: python ccurl.py url.com times")
@@ -28,12 +38,21 @@ total_count = 0
 response_codes = {}
 for idx in range(times):
     try:
-        status_code = subprocess.check_output(cmd, shell=True)
+        status_code = int(subprocess.check_output(cmd, shell=True))
+
         if not status_code in response_codes:
             response_codes[str(status_code)] = 1
         else:
             response_codes[str(status_code)] += 1
-        print("{}{} (try {}) Status: {}, Accum: {}".format(
+        if status_code >= 300 and status_code <= 499:
+            color = colors.YELLOW
+        elif status_code >= 500 and status_code <= 599:
+            color = colors.RED
+        else:
+            color = colors.ENDCOLOR
+
+        print("{}{}{} (try {}) Status: {}, Accum: {}".format(
+            color,
             url[0:20],
             '...' if len(url) > 20 else '',
             idx,
